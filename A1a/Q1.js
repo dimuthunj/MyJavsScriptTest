6bd063bbc5582642f6a71263df53c63088783bdd
Dimuthu Jayasingha <dimuthu.jayasingha@monash.edu>
2020-06-08 19:23:06 +1000

    Revert "addedcode"
    This reverts commit b69b6a6d69db26a730af6e94bc284c3ecd05a1ea.

--- A1a/Q1.js
+++ A1a/Q1.js
@@ -2,21 +2,3 @@ for(let  i=0;i<10;i++)
 {
   console.log(Math.random()*2);
 }
-dummy click function
-// This function execute when user select a location.
-function selectSource()
-{
- sourceAirport =selectedMarker;
- //remove all markers on the map.
- for(let i=0;i<markerArray.length;i++)
- {
-   markerArray[i].remove();
- }
- markerArray=[];// reset array
- // add the selected to the map
- let marker = new mapboxgl.Marker({ "color": "#2F269F" });
- console.log(sourceAirport);
- marker.setLngLat(sourceAirport.coordinates);
-   marker.addTo(map);
-
-}
